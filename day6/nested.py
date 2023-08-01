4058076e6141781f99e30c2bf54020489ceffc46
devihub23 <sarapradeep12@gmail.com>
2023-08-01 13:31:25 -0500

    nested_day6_23

---
+++ day6/nested.py
@@ -0,0 +1,10 @@
+a = input("enter the value of a:")
+b = input("enter the value of b:")
+x = int(a)
+y = int(b)
+print(x+y)
+
+
+c=int(input("enter the value of c:"))
+d=int(input("enter the value of d:"))
+print(c+d)
