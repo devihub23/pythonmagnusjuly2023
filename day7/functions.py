182400fefdc9f7d65e44c907898945a4512b490d
devihub23 <sarapradeep12@gmail.com>
2023-08-01 13:43:17 -0500

    fuctions_day6_23

---
+++ day7/functions.py
@@ -0,0 +1,23 @@
+def add():
+    a=10
+    b=20
+    print(a+b)
+add()
+def sub():
+    a=10
+    b=5
+    print(a-b)
+sub()
+def multi():
+    a=20
+    b=5
+    print(a*b)
+multi()
+
+
+def div():
+    a=25
+    b=5
+    print(a/b)
+div()
+
