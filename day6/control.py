155a5c3825878ef77c2182bf680b245fd2ad44a9
devihub23 <sarapradeep12@gmail.com>
2023-07-31 22:31:21 -0500

    control_day6_23

---
+++ day6/control.py
@@ -0,0 +1,26 @@
+a=30
+b=20
+if a>b:
+  print("greater value")
+
+   a=10
+    b=20
+if a>b:
+     print("greater value")
+
+      a=10
+       b=40
+if a>b:
+           print(" greater value")
+else:
+            print("lesser value")
+a=30
+b=20
+if a>b:
+    print("greater value")
+elif a<b:
+    print("lesser value")
+elif a==b:
+    print("equal")
+else:
+    print("invalid")
