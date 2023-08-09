fd6d70b17a9515549f12970375ebfcbfbb70bae3
devihub23 <sarapradeep12@gmail.com>
2023-08-08 23:00:05 -0500

    handon2_fifth_23

--- handson2/fifth.py
+++ handson2/fifth.py
@@ -1,6 +1,6 @@
 a=int(input("first angle of triangle a:"))
-b=int(input("first angle of triangle b:"))
-c=int(input("first angle of triangle c:"))
+b=int(input("second angle of triangle b:"))
+c=int(input("third angle of triangle c:"))
 x=a+b+c
 print(int(x))
 if x==180:
