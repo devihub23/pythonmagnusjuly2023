060d06642e7c05201b53754b6e3bc7eaebc038fd
devihub23 <sarapradeep12@gmail.com>
2023-08-14 23:53:18 -0500

    handon2_seventh_23

---
+++ handson2/seventh.py
@@ -0,0 +1,17 @@
+a=input("first number a:")
+b=input("second number b:")
+N=a+b
+print(N)
+
+a=int(input("first number a:"))
+b=int(input("second number b:"))
+X=a+b
+print(X)
+
+if (X==7 or a==7 or b==7 or int(N)%7==0 ):
+    print("special no")
+
+else:
+    print("normal no")
+
+
